0553d2a9678641d043a7048b06b8b463b2fdf6a1
Jason <jtimm@gicw.org>
2025-02-25 13:59:59 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './Header';
+import Gallery from './Gallery';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div className='App'>
+      <Header />
+        <Gallery />
+        <Footer />
+    </div>
+  );
+}
+
+export default App;
